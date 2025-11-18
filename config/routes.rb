eb18a84969cc753350dfa32b7843b937032a72cd
Rafi Fitzmaurice <rafi.fitzm@gmail.com>
2025-11-18 14:59:07 +0000

    Refactored with resources

--- config/routes.rb
+++ config/routes.rb
@@ -8,16 +8,18 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
 
-  # As a user, I can add a new task
-  get "tasks/new", to: "tasks#new", as: :new_task
-  post "tasks", to: "tasks#create"
-  # As a user, I can list tasks => tasks#index
-  get "tasks", to: "tasks#index"
-  # As a user, I can view the details of a task
-  get "tasks/:id", to: "tasks#show", as: :task
-  # As a user, I can edit a task (mark as completed / update title & details)
-  get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
-  patch "tasks/:id", to: "tasks#update"
-  # As a user, I can remove a task
-  delete "tasks/:id", to: "tasks#destroy"
+  # # As a user, I can add a new task
+  # get "tasks/new", to: "tasks#new", as: :new_task
+  # post "tasks", to: "tasks#create"
+  # # As a user, I can list tasks => tasks#index
+  # get "tasks", to: "tasks#index"
+  # # As a user, I can view the details of a task
+  # get "tasks/:id", to: "tasks#show", as: :task
+  # # As a user, I can edit a task (mark as completed / update title & details)
+  # get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+  # patch "tasks/:id", to: "tasks#update"
+  # # As a user, I can remove a task
+  # delete "tasks/:id", to: "tasks#destroy"
+
+  resources :tasks
 end
